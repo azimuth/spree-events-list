6eea0553cc488744e40edc0a6d19f34f2b3112a7
Christopher Maujean <cmaujean@gmail.com>
2010-01-03 15:35:53 -0800

    adding wymeditor to tracked files
    also, added medium image size for events and rearranged the add/edit form

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ActiveRecord::Base
   has_attached_file :image, 
                     :styles => { :large => "800x800>",
+                                 :medium => "400x400>",
                                  :thumb => "100x100>" }
 end
