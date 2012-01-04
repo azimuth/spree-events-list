ff6cfb39f4a29258c01b2351b1b02a951e74dc62
Rebekah <rebekah@azimuthinternet.com>
2012-01-03 16:12:27 -0800

    Changing the front-end event display order back to ascending

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < Spree::BaseController
   def index
-    @events = Event.find_visible_by_date(Spree::Config[:events_days_before], Spree::Config[:events_days_after]).order('begin_event DESC')
+    @events = Event.find_visible_by_date(Spree::Config[:events_days_before], Spree::Config[:events_days_after]).order('begin_event ASC')
   end
 
   def show
